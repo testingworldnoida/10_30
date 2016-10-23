fae5625fbfdabecc456dd8c80a6330ae78c7db56
TestingWorld <TestingWorld@TestingWorld-PC>
2016-10-23 11:27:50 +0530

    TASK ID-147

---
+++ SeleniumWork/src/testcases/TC_002.java
@@ -0,0 +1,25 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class TC_002 {
+	
+	@Test
+	public void tcase1() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		//driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+		driver.get("https://www.facebook.com");
+		driver.findElementById("email").sendKeys("Hello");
+	    	
+		
+	}
+
+}
